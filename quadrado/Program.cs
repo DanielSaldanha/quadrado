8b4ffc751bd4fb72c3c6e2fc306d71c6b7a7efde
DanielSaldanha <danielpedrosaldanha@gmail.com>
2024-07-02 13:57:30 -0300

    Adicionar arquivos de projeto.

---
+++ quadrado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quadrado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            retangulo p1 = new retangulo();
+            Console.WriteLine("calculo de area");
+
+            //leitura de dados do retangulo
+            Console.WriteLine("informe o valor da altura: ");
+            p1.Vertical = int.Parse(Console.ReadLine());
+            Console.WriteLine("informe o valor da base: ");
+            p1.Orizontal = int.Parse(Console.ReadLine());
+            p1.exibir();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ quadrado/retangulo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quadrado
+{
+    internal class retangulo
+    {
+		private int vertical;
+
+		public int Vertical
+		{
+			get { return vertical; }
+
+			set
+			{ 
+				if(vertical >= 0)
+				{
+					vertical = value;
+				}
+				else
+				{
+					this.vertical = 0;
+				}
+			}
+		}
+		private int orizontal;
+
+		public int Orizontal
+		{
+			get { return orizontal; }
+			set 
+			{
+                if (orizontal >= 0)
+                {
+                    orizontal = value;
+                }
+                else
+                {
+                    this.orizontal = 0;
+                }
+            }
+		}
+
+		public int Area
+		{
+			get { return Vertical * Orizontal; }
+			
+		}
+
+		public void exibir()
+		{
+			Console.WriteLine("base: " + Orizontal);
+            Console.WriteLine("altura: " + Vertical);
+            Console.WriteLine("area: " + Area);
+        }
+
+
+	}
+}
